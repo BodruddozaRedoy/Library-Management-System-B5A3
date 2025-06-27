1777366da9a3c1531e51f76aa50bde9dc39fb0a9
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-06-28 01:38:49 +0600

    feat: add update and delete endpoints for books

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -62,15 +62,61 @@ bookRoutes.get("/books", async (req: Request, res: Response) => {
   }
 });
 
+// get book by book id
 bookRoutes.get("/books/:bookId", async (req: Request, res: Response) => {
   const { bookId } = req.params;
-  console.log(bookId)
+  console.log(bookId);
   try {
-    const book = await Book.findById(bookId)
+    const book = await Book.findById(bookId);
     res.status(200).json({
       success: true,
-      message: 'Book retrieved successfully',
+      message: "Book retrieved successfully",
       data: book,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Validation failed",
+      error: error,
+    });
+    console.error(error);
+  }
+});
+
+// update a book
+bookRoutes.put("/books/:bookId", async (req: Request, res: Response) => {
+  const { bookId } = req.params;
+  const body = req.body;
+  try {
+    const updateBook = await Book.findByIdAndUpdate(bookId, body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: updateBook,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Validation failed",
+      error: error,
+    });
+    console.error(error);
+  }
+});
+
+// delete a book
+bookRoutes.delete("/books/:bookId", async (req: Request, res: Response) => {
+  const { bookId } = req.params;
+  try {
+    await Book.findByIdAndDelete(bookId);
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      data: null,
     });
   } catch (error) {
     res.status(400).json({
